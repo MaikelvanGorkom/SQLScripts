be7d5838819dd416c40db90544c1b0fb777ae15b
MaikelvanGorkom <35782401+MaikelvanGorkom@users.noreply.github.com>
2024-06-25 11:04:46 +0200

    Update and rename Record size to Record size.sql
    1. Added .sql extension
    2. removed a comment code

--- Record size.sql
+++ Record size.sql
@@ -14,7 +14,7 @@ select @sql = @sql + ' + isnull(datalength(' + QUOTENAME(name) + '), 1)'
         from  sys.columns 
         where object_id = object_id(@table)
         and   is_computed = 0
-set @sql = @sql + ') as rowsize from ' + @table -- + ' order by rowsize desc'
+set @sql = @sql + ') as rowsize from ' + @table + ' order by rowsize desc'
 
 PRINT @sql
 
